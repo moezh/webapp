4b1bda545e51e7586f1a7368a597dbe55f873c10
Moez Hachicha <moez.hachicha@icloud.com>
2026-01-03 22:31:45 +0100

    0.1.0

--- workspaces/app/astro.config.mjs
+++ workspaces/app/astro.config.mjs
@@ -2,6 +2,7 @@
 import { defineConfig } from "astro/config";
 import node from "@astrojs/node";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://astro.build/config
 export default defineConfig({
@@ -18,5 +19,10 @@ export default defineConfig({
   },
   vite: {
     plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve("./src"),
+      },
+    },
   },
 });
